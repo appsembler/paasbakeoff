623a3a552561525c17a3d49cf73e5d932e3ba088
Nate Aune <natea@jazkarta.com>
2012-11-11 13:10:53 -0500

    simplify the package installing

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='paasbakeoff',
 	packages=find_packages(),
 	include_package_data=True,
 	description='Example Mezzanine CMS deploy to OpenShift PaaS',
-	install_requires=open('%s/mywebsite/requirements/project.txt' % os.environ.get('OPENSHIFT_REPO_DIR', PROJECT_ROOT)).readlines(),
+#	install_requires=open('%s/mywebsite/requirements/project.txt' % os.environ.get('OPENSHIFT_REPO_DIR', PROJECT_ROOT)).readlines(),
+	install_requires=['Mezzanine==1.2.4',],
 )
 
