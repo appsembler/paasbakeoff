591e16a67e61235d5841a183bb4d4e8d3ae8bed2
Nate Aune <natea@jazkarta.com>
2012-11-11 13:06:10 -0500

    need to look for the requirements.txt from the repo root

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 setup(name='paasbakeoff',
 	version='1.0',
 	author='Nate Aune',
@@ -8,6 +10,6 @@ setup(name='paasbakeoff',
 	packages=find_packages(),
 	include_package_data=True,
 	description='Example Mezzanine CMS deploy to OpenShift PaaS',
-	install_requires=open('mywebsite/requirements/project.txt').readlines(),
+	install_requires=open('%s/mywebsite/requirements/project.txt' % os.environ.get('OPENSHIFT_REPO_DIR', PROJECT_ROOT)).readlines(),
 )
 
