395855322b409ac7c0586f5bff5a29b2bac9e987
Nate Aune <natea@jazkarta.com>
2012-11-12 23:48:07 -0500

    namespace the myutils package

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -282,7 +282,7 @@ INSTALLED_APPS = (
     "mezzanine.twitter",
     #"mezzanine.accounts",
     #"mezzanine.mobile",
-    "myutils",
+    "mywebsite.myutils",
 )
 
 # List of processors used by RequestContext to populate the context.
