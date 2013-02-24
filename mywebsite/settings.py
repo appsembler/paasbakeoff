2921c08ba27341bad5a41424d9fb8835c4445b0c
Nate Aune <natea@jazkarta.com>
2013-02-24 16:24:56 -0500

    only try to load the sendgrid settings if they are set

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -368,13 +368,16 @@ if os.environ.get("RACK_ENV", None) == "production":
     # SENDGRID SETTINGS #
     #####################
 
-    EMAIL_HOST = 'smtp.sendgrid.net'
-    EMAIL_HOST_USER = os.environ.get['SENDGRID_USERNAME', '']
-    EMAIL_HOST_PASSWORD = os.environ.get['SENDGRID_PASSWORD', '']
-    EMAIL_PORT = 587        # 25, 587, 2525 and 465 on ssl
-    EMAIL_USE_TLS = True
-    DEFAULT_FROM_EMAIL = 'user@domain.com'
-
+    try os.environ['SENDGRID_USERNAME']:
+        EMAIL_HOST = 'smtp.sendgrid.net'
+        EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+        EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+        EMAIL_PORT = 587        # 25, 587, 2525 and 465 on ssl
+        EMAIL_USE_TLS = True
+        DEFAULT_FROM_EMAIL = 'user@domain.com'
+    except:
+        pass
+        
 ##################
 # LOCAL SETTINGS #
 ##################
