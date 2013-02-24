bf57dfedc25fccc1fcf7fb102880d617d6420720
Nate Aune <natea@jazkarta.com>
2013-02-24 15:39:27 -0500

    avoid KeyErrors when can't find environment variables

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -369,8 +369,8 @@ if os.environ.get("RACK_ENV", None) == "production":
     #####################
 
     EMAIL_HOST = 'smtp.sendgrid.net'
-    EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-    EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+    EMAIL_HOST_USER = os.environ.get['SENDGRID_USERNAME', '']
+    EMAIL_HOST_PASSWORD = os.environ.get['SENDGRID_PASSWORD', '']
     EMAIL_PORT = 587        # 25, 587, 2525 and 465 on ssl
     EMAIL_USE_TLS = True
     DEFAULT_FROM_EMAIL = 'user@domain.com'
